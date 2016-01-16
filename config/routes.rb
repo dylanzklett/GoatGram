54c1b9e3668b0ccde9cbf3bf3e02e042e914062e
Dylan Klett <dylanzklett@gmail.com>
2016-01-16 15:11:35 -0500

    added Posts model and instanstalled Paperclip Gem. Migrated database.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'posts#index'
+
+  get '/create', to: 'posts#create'
+
+  get '/show', to: 'posts#read'
+
+  get '/update', to: 'posts#update'
+
+  get '/delete', to: 'posts#delete'
+
+  resources :posts
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
